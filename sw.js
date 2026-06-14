b3a268e4a41cc99ff1821c81bcf6fa93f23ff55c
kjsh1028 <kjsh1028@gmail.com>
2026-06-14 09:12:23 +0800

    fix: bump sw version to force reinstall

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-// Service Worker - always fetch fresh
+// Service Worker v9.2 - always fetch fresh
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => { 
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
